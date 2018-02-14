6e69129fac8f19eefc6df0063f5dac09936acd0a
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-02-14 11:01:28 +0300

    ortak eklendi

---
+++ hesapmakinesi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapmakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1,sayi2;
+            Console.Write("1. sayıyı giriniz :");
+            sayi1=Convert.ToDouble(Console.ReadLine());
+            Console.Write("2. sayıyı giriniz :");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+        }
+   }
+}
