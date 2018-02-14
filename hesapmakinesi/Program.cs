6fdcee9e01c6a11add9dcf8508f33527513aa7f7
yozdel <ozdelyasin@gmail.com>
2018-02-14 14:17:54 +0300

    işlem Koşulları
    else if

--- hesapmakinesi/Program.cs
+++ hesapmakinesi/Program.cs
@@ -10,11 +10,29 @@ namespace hesapmakinesi
     {
         static void Main(string[] args)
         {
-            double sayi1,sayi2;
+            double sayi1,sayi2; char islem;
             Console.Write("1. sayıyı giriniz :");
             sayi1=Convert.ToDouble(Console.ReadLine());
             Console.Write("2. sayıyı giriniz :");
             sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("işlemi giriniz :");
+            islem = Convert.ToChar(Console.ReadLine());
+            if (islem == '+')
+            {
+
+            }
+            else if (islem == '-')
+            {
+
+            }
+            else if (islem == '*')
+            {
+
+            }
+            else if (islem == '/')
+            {
+
+            }
         }
    }
 }
